9c8a32d198dfacf3a29645bec85356af4166d8bf
tientvv <tientranvan19052004@gmail.com>
2025-09-18 16:09:56 +0700

    Update script.js

--- JavaScriptFundamentalsPart1/let-const-var/script.js
+++ JavaScriptFundamentalsPart1/let-const-var/script.js
@@ -1,18 +1,18 @@
-/*
+// 1. let - for variables that can change (mutable)
 let age = 30;
-age = 31;
+age = 31; // Reassigning is allowed
 
+// 2. const - for variables that shouldn't change (immutable)
 const birthYear = 2004;
-// birthYear = 2003;
+// birthYear = 2003; // Error! Cannot reassign
+// const job; // Error! Must be initialized
 
-// const job;
-
+// 3. var - old way of defining variables (avoid using)
 var job = 'programmer';
-job = 'teacher';
+job = 'software engineer'; // Reassigning is allowed
 
 lastName = 'Tran';
-console.log(lastName);
-*/
+console.log(lastName); // Can be reassigned like let
 
 // Exercise 2
 let language;
